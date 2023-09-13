b2d0e4603bd627d310516e17b9ddab9116cdd418
Aravind--1187 <f20231187@hyderabad.bits-pilani.ac.in>
2023-09-13 18:55:15 +0530

    Changed CSV to SQL database

--- Court Booking.py
+++ Court Booking.py
@@ -1,48 +1,56 @@
-import csv
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Madras123$",
+    database="Court_Booking"
+)
 
-court = 'Badminton Court.csv'
-ch = 'Y'
-list_index = 0
-days = ['MONDAY', 'TUESDAY', 'WEDNESDAY',
-        'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+mycursor = mydb.cursor()
+court = 'badminton'
+while (True):
 
-while (ch == 'Y'):
-    with open(court) as csvfile:
-        csvreader = csv.reader(csvfile)
-        court = court.rstrip('.csv')
-        print(court, 'Booking')
-
-        booking_date = input(
-            '\nEnter day of the week to be booked: (Eg: Monday): ')
-        list_index = days.index(booking_date.upper())
-        day_index = list_index+2
-        print('\nAvailable timeslots:\n')
-        for row_list in csvreader:
-            if (row_list[day_index] == 'Empty'):
-                print(row_list[0], 'to', row_list[1])
-        booking_start_time = input(
-            '\nEnter time of the day to be booked: (Eg: 06:00): ')
-        booking_time_int = int(
-            booking_start_time[0:2])*100 + int(booking_start_time[3:])
-
-        print('Confirming details:')
-        print('Chosen day slot:', booking_date.title())
-        print('Chosen time slot:', booking_start_time)
-        ch = input('Confirm booking? (Y/N) ').upper()
-        if (ch == 'N'):
-            break
-        id = input('Enter your ID Number: ').upper()
-        name = input('Enter your Name: ')
-        if (' ' in name and id[-1] == 'H' and len(id) == 13):
-            print('Booking Confirmed')
-
-    ch = input('Continue booking? (Y/N) ').upper()
-    if (ch == 'N'):
-        break
     print('1. Badminton Court')
     n = int(input('Enter index of court: '))
+    print()
     if (n == 1):
-        court = 'Badminton Court.csv'
+        court = 'Badminton'
     else:
-        ch = 'N'
+        break
+
+    print(court.title(), 'Court Booking')
+
+    booking_date = input(
+        '\nEnter day of the week to be booked: (Eg: Monday): ').title()
+    print('\nAvailable timeslots:\n')
+    mycursor.execute("SELECT StartTime, EndTime FROM %s where %s = 'Empty';",
+                     court, booking_date)  # Working on this error
+
+    table = mycursor.fetchall()
+    for rows in table:
+        print(rows[0], rows[1], sep=' to ')
+    booking_timeslot = input(
+        '\nEnter time of the day to be booked: (Eg: 06:00): ')
+
+    print('Confirming details:')
+    print('Chosen day slot:', booking_date)
+    print('Chosen time slot:', booking_timeslot)
+    ch = input('Confirm booking? (Y/N) ').upper()
+    if (ch == 'N'):
+        break
+    id = input('Enter your ID Number: ').upper()
+    name = input('Enter your Name: ')
+    if (' ' in name and id[-1] == 'H' and len(id) == 13):
+        print('ID Confirmed.')
+        mycursor.execute('UPDATE %s SET %s = %s WHERE StartTime = %s',
+                         court, booking_date, id, booking_timeslot)
+        print('Booking Confirmed.')
+
+    ch = input('Continue booking? (Y/N) ').upper()
+    if (ch == 'Y'):
+        continue
+    else:
+        break
+
+print('End of program.')
